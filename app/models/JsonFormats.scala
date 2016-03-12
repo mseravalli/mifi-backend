309548898d9ddf50035cb6ceda27fd692c04129e
Marco Seravalli <marco.seravalli@gmail.com>
2016-03-12 17:50:49 +0100

    update Formatters

--- app/models/JsonFormats.scala
+++ app/models/JsonFormats.scala
@@ -2,9 +2,11 @@ package models
 
 object JsonFormats {
   import play.api.libs.json.Json
+  import models.Tables.AccountsRow
   import models.Tables.TransactionsRow
 
-  implicit val accountFmt = Json.format[Account]
+  implicit val accountFmtOld = Json.format[Account]
+  implicit val accountFmt = Json.format[AccountsRow]
   implicit val categoryFmt = Json.format[Category]
   implicit val transactionFmt = Json.format[TransactionsRow]
 }
