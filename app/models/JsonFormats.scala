6bcbfb139f01f57b69ceca6e7bb3bcccbb485bad
Marco Seravalli <marco.seravalli@gmail.com>
2016-08-18 23:28:05 +0200

    add formatter for java.sql.Date

--- app/models/JsonFormats.scala
+++ app/models/JsonFormats.scala
@@ -1,11 +1,29 @@
 package models
 
+
 object JsonFormats {
-  import play.api.libs.json.Json
+  import play.api.libs.json._
   import models.Tables.AccountsRow
   import models.Tables.TransactionsRow
+  import java.sql.Date
 
-  implicit val accountFmtOld = Json.format[Account]
+  implicit val sqlDateFmt = new Format[Date] {
+    val sdf = new java.text.SimpleDateFormat("yyyy-MM-dd")
+    val sdfPattern = "\\b(0[1-9]|[1-3][0-9])-(0[1-9]|1[0-2])-[1-9][0-9]{3}\\b"
+    def reads(json: JsValue):JsResult[Date] = json match {
+      case JsString(d) => {
+        val theDate = new Date(sdf.parse(d).getTime)
+        if (d.matches(sdfPattern) && theDate.compareTo(new Date(0)) > 0)
+          JsSuccess(new Date(sdf.parse(d).getTime))
+        else
+          JsError("Validate error expected date in format 'dd-MM-yyyy'")
+      }
+      case _ => JsError("Validate error expected date in String")
+    }
+
+    def writes(sd: Date): JsValue = JsString(sdf.format(sd))
+  }
+
   implicit val accountFmt = Json.format[AccountsRow]
   implicit val categoryFmt = Json.format[Category]
   implicit val transactionFmt = Json.format[TransactionsRow]
