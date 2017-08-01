8a9a3df03a61755f351fac3f12ba3a03482ac8af
Marco Seravalli <marco.seravalli@accenture.com>
2017-08-01 22:15:01 +0200

    update controller to use persistence framework and remove unused enpoints

--- app/controllers/TransactionController.scala
+++ app/controllers/TransactionController.scala
@@ -33,109 +33,18 @@ object TransactionController {
       .result
   }
 
-  val readTransactionQuery = s"""
-    SELECT *
-    FROM transactions
-    WHERE id = ?
-  """
   def readTransactionQuery2(id: Long) = Tables.Transactions.filter(t => t.id === id).result
 
-  val updateTransactionQuery = s"""
-    UPDATE transactions
-    SET category = ? , sub_category = ?
-    WHERE id = ?
-  """
-
-  val updateAmountQuery = s"""
-    UPDATE transactions
-    SET amount = ?
-    WHERE id = ?
-  """
-
-  val insertTransactionQuery = s"""
-    INSERT INTO transactions ("account_number",
-      "transaction_date",
-      "exchange_date",
-      "receiver",
-      "purpose",
-      "amount",
-      "currency",
-      "category",
-      "sub_category",
-      "approved",
-      "comment")
-    VALUES (?,?,?,?,?,?,?,?,?,?,?)
-    RETURNING id
-  """
+  def updateTransactionQuery(id: Long, category: String, subCategory: String) = {
+    Tables.Transactions.filter(t => t.id === id)
+      .map(x => (x.category, x.subCategory ))
+      .update(Some(category), Some(subCategory))
+  }
 }
 
 @Singleton
 class TransactionController extends Controller{
   import models.JsonFormats
-
-  /**
-   * Splits a transaction in a left and a right part
-   * Only the amount and the description for the r part are mandatory
-   * The description for the left part is optional and the amount is computed
-   * example:
-     {
-       "id": 4238,
-       "rAmount": -55,
-       "rCategory": "free time",
-       "rSubCategory": "going out",
-       "rComment": "comment"
-     }
-   * @return
-   */
-  def splitTransaction(id: String): Action[JsValue] =  Action.async(parse.json){ request => async {
-    val jsonRequest = request.body
-    val lId = (jsonRequest \ "id").as[BigDecimal]
-    val rAmount:BigDecimal = (jsonRequest \ "rAmount").as[BigDecimal]
-    val rCategory = (jsonRequest \ "rCategory").as[String]
-    val rSubCategory = (jsonRequest \ "rSubCategory").as[String]
-    val rComment = (jsonRequest \ "rComment").as[String]
-
-    await { Global.pool.inTransaction{ c =>
-      c.sendPreparedStatement(TransactionController.readTransactionQuery, Array(lId)).flatMap{ res =>
-        val account_number   = res.rows.head.map(r => r("account_number")).head.asInstanceOf[String]
-        val transaction_date = res.rows.head.map(r => r("transaction_date")).head.asInstanceOf[LocalDate]
-        val exchange_date    = res.rows.head.map(r => r("exchange_date")).head.asInstanceOf[LocalDate]
-        val receiver         = res.rows.head.map(r => r("receiver")).head.asInstanceOf[String]
-        val purpose          = res.rows.head.map(r => r("purpose")).head.asInstanceOf[String]
-        val amount           = res.rows.head.map(r => r("amount")).head.asInstanceOf[BigDecimal]
-        val currency         = res.rows.head.map(r => r("currency")).head.asInstanceOf[String]
-        val approved         = res.rows.head.map(r => r("approved")).head.asInstanceOf[Boolean]
-
-        if (rAmount.abs > amount.abs || rAmount * amount <= 0) {
-          Future(BadRequest(Json.obj("msg" -> JsString("Wrong amount passed"))))
-        }
-        else {
-          val lAmount = amount - rAmount
-          val updateValues = Array(lAmount, lId)
-          c.sendPreparedStatement(TransactionController.updateAmountQuery, updateValues).flatMap { res =>
-            val insertValues = Array(account_number,
-              transaction_date,
-              exchange_date,
-              receiver,
-              purpose,
-              rAmount,
-              currency,
-              rCategory,
-              rSubCategory,
-              approved,
-              rComment
-            )
-            c.sendPreparedStatement(TransactionController.insertTransactionQuery, insertValues).flatMap{ r =>
-              r.rows match {
-                case Some(rows) => Future(Ok(Json.obj("msg" -> "Split completed correctly", "transactions" ->  Json.arr(lId.toString, rows.head.apply(0).toString) )))
-                case None => Future(InternalServerError(Json.obj("msg" -> "No Id was returned")))
-              }
-            }
-          }
-        }
-      }
-    }}
-  }}
 
   def readTransactions(): Action[AnyContent] = Action.async { request => async {
     val startDate = Date.valueOf(request.getQueryString("startDate").getOrElse("1900-01-01"))
@@ -160,10 +69,8 @@ class TransactionController extends Controller{
     val category = (jsonRequest \ "category").as[String]
     val subCategory =   (jsonRequest \ "subCategory").as[String]
 
-    val queryValues = Array(category, subCategory, id)
-    val query = TransactionController.updateTransactionQuery
-    val queryResult = await { Global.pool.sendPreparedStatement(query, queryValues)}
+    val res = await { Global.db.run(TransactionController.updateTransactionQuery(id.toLong, category, subCategory)) }
 
-    Ok(Json.obj("msg" -> queryResult.statusMessage))
+    Ok(Json.obj("result" -> JsString(res.toString)))
   }}
 }
