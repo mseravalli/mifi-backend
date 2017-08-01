706598d89ad799d0d02f93e04bd66004a91cac37
Marco Seravalli <marco.seravalli@accenture.com>
2017-08-01 14:10:20 +0200

    use only persistence framework for sub categories

--- app/controllers/SubCategoryController.scala
+++ app/controllers/SubCategoryController.scala
@@ -9,7 +9,6 @@ import com.github.mauricio.async.db.util.ExecutorServiceUtils.CachedExecutionCon
 import models.{Category, JsonFormats, Tables}
 import javax.inject.Singleton
 
-import org.joda.time.LocalDate
 import org.slf4j.{Logger, LoggerFactory}
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.mvc._
@@ -21,99 +20,28 @@ import slick.driver.PostgresDriver.api._
 
 
 object SubCategoryController {
-  def getSubCategoryTransactions(startDate: Date, endDate: Date, categories: String, subCategories: Array[String]) = {
+  def getSubCategoryTransactions(startDate: Date, endDate: Date, category: String, subCategories: Array[String]) = {
     Tables.Transactions
       .filter(t => t.transactionDate > startDate && t.transactionDate < endDate
-        && subCategories.foldLeft(t.subCategory =!= t.subCategory)((res, sc)=> res || ( (t.category like categories) && (t.subCategory like sc) ) )
+        && subCategories.foldLeft(t.subCategory =!= t.subCategory)((res, sc)=> res || ( (t.category like category) && (t.subCategory like sc) ) )
       )
       .map{x => (x.transactionDate, x.subCategory, x.amount)}
       .result
   }
 
-  val getSubCategoriesQuery = """
-    SELECT sub_category, color
-    FROM category_match
-    WHERE category = ?
-    ORDER BY sub_category
-                              """
-  def getSubCategories(categories: Seq[String]): Future[Map[String,String]] = { async {
-    val queryResult = await { Global.pool.sendPreparedStatement(getSubCategoriesQuery, categories) }
-    val res = queryResult.rows.map { rows => rows.map{ r =>
-      (r("sub_category").asInstanceOf[String] -> r("color").asInstanceOf[String])
-    }.toMap}
-    res.getOrElse(Map[String,String]())
-  }}
-
-  // the flow can be eithen in or out
-  def totalFlowSubCatQuery(dateFormat: String, flow:String, subCategories: Array[String]): String = {
-    val op = flow match { case "in" => ">" case "out" => "<" }
-    val format = Formatter.normalizeDateFormat(dateFormat)
-    var subCat = "FALSE"
-    for (c <- subCategories) { subCat += " OR t_0.sub_category = ?" }
-    s"""
-    SELECT
-      t_0.sub_category AS t_0_sub_category,
-      ABS(SUM(t_0.amount)) AS t_0_amount
-    FROM transactions t_0
-    WHERE
-      (t_0.transaction_date BETWEEN ? AND ?)
-      AND
-      (t_0.category = ?)
-      AND
-      ($subCat)
-    GROUP BY t_0_sub_category
-    HAVING SUM((t_0.amount)) $op 0
-    ORDER BY t_0_amount DESC
-    """
-  }
-
-  def aggregateSubCategoryQuery(dateFormat: String,
-                                 categories: Array[String],
-                                 subCategories: Array[String]): String = {
-    val format = Formatter.normalizeDateFormat(dateFormat)
-    var cat = "FALSE"
-    for (c <- categories) { cat += " OR t_0.category = ?" }
-    var subCat = "FALSE"
-    for (c <- subCategories) { subCat += " OR t_0.sub_category = ?" }
-    s"""
-    WITH cat AS (
-        SELECT
-          to_char(t_0.transaction_date,'$format') AS t_0_date,
-          t_0.category AS t_0_super_category,
-          t_0.sub_category AS t_0_category,
-          SUM((t_0.amount)) AS t_0_amount
-        FROM transactions t_0
-        WHERE
-          (t_0.transaction_date BETWEEN ? AND ?)
-          AND
-          ($cat)
-          AND
-          ($subCat)
-        GROUP BY t_0_date, t_0.category, t_0.sub_category
-        ORDER BY t_0_date, t_0.category, t_0.sub_category
-      ),
-      total AS (
-        SELECT
-          to_char(t_0.transaction_date,'$format') AS t_0_date,
-          text('total') AS t_0_super_category,
-          text('total') AS t_0_category,
-          SUM((t_0.amount)) AS t_0_amount
-        FROM transactions t_0
-        WHERE
-          (t_0.transaction_date BETWEEN ? AND ?)
-          AND
-          ($cat)
-          AND
-          ($subCat)
-        GROUP BY t_0_date
-        ORDER BY t_0_date
+  // the flow can be either in or out
+  def totalFlowSubCatQuery(startDate: Date, endDate: Date, flow: String, category: String, subCategories: Array[String]) = {
+    Tables.Transactions
+      .filter(t => t.transactionDate > startDate && t.transactionDate < endDate
+        && subCategories.foldLeft(t.subCategory =!= t.subCategory)((res, sc)=> res || ( (t.category like category) && (t.subCategory like sc) ) )
       )
-    SELECT * FROM cat
-    UNION
-    SELECT * FROM total
-    ORDER BY t_0_date, t_0_category
-    """
+      .groupBy(_.subCategory)
+      .map(x => (x._1, x._2.map(_.amount).sum))
+      .filter(x => flow match { case "in" => x._2 > BigDecimal(0) case "out" => x._2 < BigDecimal(0) } )
+      .sortBy(_._1.asc)
+      .result
   }
+
 }
 @Singleton
 class SubCategoryController extends Controller {
@@ -121,26 +49,8 @@ class SubCategoryController extends Controller {
 
   private final val logger: Logger = LoggerFactory.getLogger(classOf[SubCategoryController])
 
-   def readSubCategories(): Action[JsValue] = Action.async(parse.json){ request => async {
-     val jsonRequest = request.body
-     val categories = (jsonRequest \ "categories").as[Array[String]]
-     val subCategories = await {getSubCategories(categories)}
-     var res: JsObject = Json.obj()
-     for (s <- subCategories) {
-      res = res + (s._1 -> JsString(s._2))
-     }
-     Ok( Json.obj("subCategories" -> res) )
-   }}
-
   /**
    * Request example:
-   *  {
-   *    "sumRange": "YYYY-MM-DD",
-   *    "startDate": "2015-01-01",
-   *    "endDate": "2015-01-31",
-   *    "categories": ["living", "free time"],
-   *    "subCategories": ["sport"]
-   *  }
    */
   def aggregateSubCategory(): Action[AnyContent] = Action.async { request => async {
     val dateFormat = Formatter.normalizeDateFormat(request.getQueryString("sumRange").getOrElse(""))
@@ -171,30 +81,24 @@ class SubCategoryController extends Controller {
     Ok( series )
   }}
 
-  def totalFlowSubCat(flow: String): Action[JsValue] = Action.async(parse.json){ request => async {
-    val jsonRequest = request.body
-    val dateFormat = Formatter.normalizeDateFormat((jsonRequest \ "sumRange").as[String])
-    val startDate = new LocalDate((jsonRequest \ "startDate").as[String])
-    val endDate =   new LocalDate((jsonRequest \ "endDate").as[String])
-    val categories = (jsonRequest \ "categories").as[Array[String]]
-    val subCategories = (jsonRequest \ "subCategories").as[Array[String]]
-    val cols = Json.arr("subCategory", "amount")
-    val result = categories.length match {
-      case 1 => {
-        val insertValues = Array(startDate, endDate) ++ categories ++  subCategories
-        val query = totalFlowSubCatQuery(dateFormat, flow, subCategories)
-        val queryResult = await { Global.pool.sendPreparedStatement(query, insertValues) }
+  def totalFlowSubCat(flow: String): Action[AnyContent] = Action.async{ request => async {
+    val dateFormat = Formatter.normalizeDateFormat(request.getQueryString("sumRange").getOrElse(""))
+    val startDate = Date.valueOf(request.getQueryString("startDate").getOrElse("1900-01-01"))
+    val endDate =   Date.valueOf(request.getQueryString("endDate").getOrElse("2100-12-31"))
+    val categories = request.getQueryString("categories").getOrElse("").split(",").map(_.trim).sorted
+    val subCategories = request.getQueryString("subCategories").getOrElse("").split(",").map(_.trim).sorted
 
-        val rows = JsArray(queryResult.rows.get.map{x =>
-          Json.arr(x("t_0_sub_category").asInstanceOf[String], x("t_0_amount").asInstanceOf[BigDecimal])
-        })
-        (cols +: rows)
-      }
-      case _ => Json.arr(cols)
+    val totalFlow = await {
+      Global.db.run(SubCategoryController.totalFlowSubCatQuery(startDate, endDate, flow, categories.head, subCategories))
     }
 
-    Ok( Json.obj("data" -> result) )
+    val cols = Json.arr("subCategory", "amount")
+    val rows = JsArray( totalFlow.map{x =>
+      Json.arr(JsString(x._1.getOrElse("")), JsNumber(x._2.map(_.abs).getOrElse(BigDecimal(0.0))))
+    })
+
+    Ok( Json.obj("data" -> (cols +: rows)) )
   }}
-  def totalFlowSubCatIn(): Action[JsValue] =  totalFlowSubCat("in")
-  def totalFlowSubCatOut(): Action[JsValue] = totalFlowSubCat("out")
+  def totalFlowSubCatIn():  Action[AnyContent] = totalFlowSubCat("in")
+  def totalFlowSubCatOut(): Action[AnyContent] = totalFlowSubCat("out")
 }
