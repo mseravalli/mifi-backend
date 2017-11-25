975d55a039cc6669f28929184f01833fccaf84a6
Marco Seravalli <marco.seravalli@tum.de>
2017-11-25 16:22:02 +0100

    update creation of testing transactions

--- build-image.sh
+++ build-image.sh
@@ -3,4 +3,5 @@ sbt dist
 export VERSION=`grep 'version :=' build.sbt | awk '{print $3}' | sed 's/\"//g'`
 unzip ./target/universal/mi-fi-$VERSION.zip -d ./target/universal/
 mv target/universal/mi-fi-$VERSION target/universal/mi-fi
-docker build -t mseravalli/mi-fi:$VERSION .
+docker build -t mseravalli/mifi:$VERSION .
+docker build -t mseravalli/postgres:$VERSION sql
---
+++ sql/initdb.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+psql -f /opt/sql/create_database.sql
+psql -U mifi -f /opt/sql/create_tables.sql
+psql -U mifi -f /opt/sql/populate_categories.sql
+psql -U mifi -f /opt/sql/insert_db.sql
+psql -U mifi -f /opt/sql/insert_kalixa.sql
+psql -U mifi -f /opt/sql/insert_hvb.sql
+psql -U mifi -f /opt/sql/insert_number26.sql
+psql -U mifi -f /opt/sql/test_transactions.sql
+
