00e6e9947fb841d6bdc7b6b7dda3c736bc518529
Marco Seravalli <marco.seravalli@tum.de>
2018-04-08 12:03:37 +0200

    hadle empty list as None

--- app/controllers/ImportController.scala
+++ app/controllers/ImportController.scala
@@ -33,8 +33,8 @@ class ImportController @Inject() (implicit ec: ExecutionContext,
     extends AbstractController(cc) with HasDatabaseConfigProvider[JdbcProfile] {
 
   def getAmount(x: List[String], a: AccountsRow): BigDecimal = {
-    val in = BigDecimal.apply(x.lift(a.amountInPos).map{amount => Formatter.formatAmount(amount)}.getOrElse("0.00"))
-    val out = BigDecimal.apply(Formatter.formatAmount(x.lift(a.amountOutPos).getOrElse("0.00")).replace("-", ""))
+    val in = BigDecimal.apply(x.lift(a.amountInPos).map{amount => Formatter.formatAmount(amount)}.filter(!_.equals("")).getOrElse("0.00"))
+    val out = BigDecimal.apply(Formatter.formatAmount(x.lift(a.amountOutPos).filter(!_.equals("")).getOrElse("0.00")).replace("-", ""))
     if (in.abs == out.abs) {
       in
     }
