c466d3595511fbcf8906ddad43860738125da97d
Marco Seravalli <marco.seravalli@tum.de>
2018-12-02 18:23:11 +0100

    update test to include hvb-depot

--- test/controllers/ApplicationIT.scala
+++ test/controllers/ApplicationIT.scala
@@ -67,17 +67,19 @@ class ApplicationIT extends PlaySpecification with JsonMatchers {
       contentAsString(response) must /("data") /# 0 /# 1 /("bcard")
       contentAsString(response) must /("data") /# 0 /# 2 /("db")
       contentAsString(response) must /("data") /# 0 /# 3 /("hvb")
-      contentAsString(response) must /("data") /# 0 /# 4 /("kalixa")
-      contentAsString(response) must /("data") /# 0 /# 5 /("n26")
-      contentAsString(response) must /("data") /# 0 /# 6 /("total")
+      contentAsString(response) must /("data") /# 0 /# 4 /("hvb-depot")
+      contentAsString(response) must /("data") /# 0 /# 5 /("kalixa")
+      contentAsString(response) must /("data") /# 0 /# 6 /("n26")
+      contentAsString(response) must /("data") /# 0 /# 7 /("total")
 
       contentAsString(response) must /("data") /# 25 /# 0 /("2016-01")
       contentAsString(response) must /("data") /# 25 /# 1 /(543.87)
       contentAsString(response) must /("data") /# 25 /# 2 /(627.95)
       contentAsString(response) must /("data") /# 25 /# 3 /(1359.73)
-      contentAsString(response) must /("data") /# 25 /# 4 /(-221.03)
-      contentAsString(response) must /("data") /# 25 /# 5 /(4414.14)
-      contentAsString(response) must /("data") /# 25 /# 6 /(6724.66)
+      contentAsString(response) must /("data") /# 25 /# 4 /(0)
+      contentAsString(response) must /("data") /# 25 /# 5 /(-221.03)
+      contentAsString(response) must /("data") /# 25 /# 6 /(4414.14)
+      contentAsString(response) must /("data") /# 25 /# 7 /(6724.66)
     }
 
     "retrieve timeseries for single account: db" in new WithApplication {
