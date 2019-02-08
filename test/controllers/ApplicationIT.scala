a6f7584b43058f2865703230bd154b6f6ea7d8c7
Marco Seravalli <seravalli@google.com>
2019-02-08 17:29:53 +0100

    change healthcheck to healthz

--- test/controllers/ApplicationIT.scala
+++ test/controllers/ApplicationIT.scala
@@ -15,7 +15,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 class ApplicationIT extends PlaySpecification with JsonMatchers {
   "Health" should {
     "be healthy" in new WithApplication() {
-      val request = FakeRequest(GET, "/health")
+      val request = FakeRequest(GET, "/healthz")
       val response = route(app, request).get
       status(response) must equalTo(OK)
       contentType(response) must beSome.which(_ == "text/plain")
