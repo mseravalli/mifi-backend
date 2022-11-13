c49bc87460769665f97a4526e0147bd605137882
Marco Seravalli <marco.seravalli@gmail.com>
2022-11-13 13:12:26 +0100

    Try to improve test redability

--- test/controllers/ImportControllerIT.scala
+++ test/controllers/ImportControllerIT.scala
@@ -12,10 +12,10 @@ class ImportControllerIT extends PlaySpecification with JsonMatchers {
       val accountId = 1
       val filePath = "test/db.csv"
 
-      val amountDeleted = 10.10
+      val netImportedAmount = -10.10
       val balanceRequest = FakeRequest(GET, s"/accounts/${accountId}?endDate=2100-12-31")
       val balanceResponse = route(app, balanceRequest).get
-      val balance = BigDecimal((contentAsJson(balanceResponse) \ "balance").get.toString())
+      val initialBalance = BigDecimal((contentAsJson(balanceResponse) \ "balance").get.toString())
 
       val file = play.api.libs.Files.SingletonTemporaryFileCreator.create(path = java.nio.file.Paths.get(filePath))
       val part = FilePart[TemporaryFile](key = "csv", filename = "the.file", contentType = None, ref = file)
@@ -29,7 +29,7 @@ class ImportControllerIT extends PlaySpecification with JsonMatchers {
       val response = route(app, request).get
       status(response) must equalTo(OK)
       contentAsString(response) must /("account") / ("account" -> accountId)
-      val finalBalanceAsString = (balance - amountDeleted).toString
+      val finalBalanceAsString = (initialBalance + netImportedAmount).toString
       contentAsString(response) must /("account") / ("balance" -> (finalBalanceAsString))
     }
 
@@ -37,10 +37,10 @@ class ImportControllerIT extends PlaySpecification with JsonMatchers {
       val accountId = 3
       val filePath = "test/hvb.csv"
 
-      val amountDeleted = BigDecimal(10.10)
+      val netImportedAmount = BigDecimal(-10.10)
       val balanceRequest = FakeRequest(GET, s"/accounts/${accountId}?endDate=2100-12-31")
       val balanceResponse = route(app, balanceRequest).get
-      val balance = BigDecimal((contentAsJson(balanceResponse) \ "balance").get.toString())
+      val initialBalance = BigDecimal((contentAsJson(balanceResponse) \ "balance").get.toString())
 
       val file = play.api.libs.Files.SingletonTemporaryFileCreator.create(path = java.nio.file.Paths.get(filePath))
       val part = FilePart[TemporaryFile](key = "csv", filename = "the.file", contentType = None, ref = file)
@@ -54,7 +54,7 @@ class ImportControllerIT extends PlaySpecification with JsonMatchers {
       val response = route(app, request).get
       status(response) must equalTo(OK)
       contentAsString(response) must /("account") / ("account" -> accountId)
-      val finalBalanceAsString = (balance - amountDeleted).toString
+      val finalBalanceAsString = (initialBalance + netImportedAmount).toString
       contentAsString(response) must /("account") / ("balance" -> (finalBalanceAsString))
     }
 
@@ -62,10 +62,10 @@ class ImportControllerIT extends PlaySpecification with JsonMatchers {
       val accountId = 7
       val filePath = "test/dkb.csv"
 
-      val amountDeleted = BigDecimal(20.20)
+      val netImportedAmount = BigDecimal(-20.20)
       val balanceRequest = FakeRequest(GET, s"/accounts/${accountId}?isSharingRatioEnabled=False&endDate=2100-12-31")
       val balanceResponse = route(app, balanceRequest).get
-      val balance = BigDecimal((contentAsJson(balanceResponse) \ "balance").get.toString())
+      val initialBalance = BigDecimal((contentAsJson(balanceResponse) \ "balance").get.toString())
 
       val file = play.api.libs.Files.SingletonTemporaryFileCreator.create(path = java.nio.file.Paths.get(filePath))
       val part = FilePart[TemporaryFile](key = "csv", filename = "the.file", contentType = None, ref = file)
@@ -79,7 +79,7 @@ class ImportControllerIT extends PlaySpecification with JsonMatchers {
       val response = route(app, request).get
       status(response) must equalTo(OK)
       contentAsString(response) must /("account") / ("account" -> accountId)
-      val finalBalanceAsString = (balance - amountDeleted).toString
+      val finalBalanceAsString = (initialBalance + netImportedAmount).toString
       contentAsString(response) must /("account") / ("balance" -> (finalBalanceAsString))
     }
 
@@ -87,10 +87,10 @@ class ImportControllerIT extends PlaySpecification with JsonMatchers {
       val accountId = 8
       val filePath = "test/dkb-cc.csv"
 
-      val amountDeleted = BigDecimal(20.20)
+      val netImportedAmount = BigDecimal(-20.20)
       val balanceRequest = FakeRequest(GET, s"/accounts/${accountId}?isSharingRatioEnabled=False&endDate=2100-12-31")
       val balanceResponse = route(app, balanceRequest).get
-      val balance = BigDecimal((contentAsJson(balanceResponse) \ "balance").get.toString())
+      val initialBalance = BigDecimal((contentAsJson(balanceResponse) \ "balance").get.toString())
 
       val file = play.api.libs.Files.SingletonTemporaryFileCreator.create(path = java.nio.file.Paths.get(filePath))
       val part = FilePart[TemporaryFile](key = "csv", filename = "the.file", contentType = None, ref = file)
@@ -104,7 +104,7 @@ class ImportControllerIT extends PlaySpecification with JsonMatchers {
       val response = route(app, request).get
       status(response) must equalTo(OK)
       contentAsString(response) must /("account") / ("account" -> accountId)
-      val finalBalanceAsString = (balance - amountDeleted).toString
+      val finalBalanceAsString = (initialBalance + netImportedAmount).toString
       contentAsString(response) must /("account") / ("balance" -> (finalBalanceAsString))
     }
   }
